fa4ee7c58ff81c62a2803a9095f1ab8b9623d2a8
Aziz <nasrulaev.a@usoft.uz>
2023-10-30 15:46:57 +0500

    fixed bugs

---
+++ database/migrations/2023_10_27_160324_change_column_to_products_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use App\Models\Admin\Brand;
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->dropForeign('products_brand_id_foreign');
+            $table->dropColumn('brand_id');
+//            $table->bigInteger('brand_id')->nullable();
+//            $table->foreignIdFor(Brand::class)
+//                ->nullable()
+//                ->constrained()
+//                ->cascadeOnDelete()
+//                ->cascadeOnUpdate();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('products', function (Blueprint $table) {
+            //
+        });
+    }
+};
