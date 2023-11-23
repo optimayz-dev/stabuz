8a4936aca6f5a4655b27cf7572549bca2b825ee6
Aziz <nasrulaev.a@usoft.uz>
2023-11-23 16:57:47 +0500

    fixed bugs

--- app/Http/Controllers/Front/CategoryController.php
+++ app/Http/Controllers/Front/CategoryController.php
@@ -68,7 +68,7 @@ class CategoryController extends Controller
 
         $products = Product::query()->whereHas('categories.translations', function ($query) use ($slug) {
             $query->where('slug', $slug);
-        })->with(['translations', 'brand', 'brand.translations', 'images'])->paginate(1);
+        })->with(['translations', 'brand', 'brand.translations', 'images'])->paginate(30);
 
 
         return view('front.category', ['category' => $category, 'products' => $products]);
